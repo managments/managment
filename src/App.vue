7ad63ccf0ea0c4cfbb8f08344d07c036972ef578
liudong <1418275530@qq.com>
2017-10-30 18:26:02 +0800

    模拟数据

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,12 @@
 <template>
   <div id="app">
-        <Heads></Heads>
-        <div class="container-fluid">
-          <div class="row" style="padding-top:52px">
-            <div class="col-md-2" style="padding:0">
-              <Silde></Silde>
-            </div>
-            <div class="col-md-10">
-                <router-view></router-view>
-            </div>
-          </div>
-        </div>
-        <Foot1></Foot1>
+      <router-view></router-view>
   </div>
 </template>
 
 <script>
-import Heads from './components/heads'
-import Silde from './components/silde'
-import Foot1 from './components/foot1'
 export default {
   name: 'app',
-  components:{
-    Heads,Silde,Foot1
-  }
 }
 </script>
 
@@ -34,5 +17,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  height: 100%;
 }
 </style>
