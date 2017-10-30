fa2919b588ddebcc7ed4854bf78ca5960a467199
liudong <1418275530@qq.com>
2017-10-30 22:20:26 +0800

    60s倒计时

--- src/components/login1.vue
+++ src/components/login1.vue
@@ -1,10 +1,14 @@
 <template>
     <div class="login1">
+
         <ziksang-count-down :start='start' @countDown ='start=false' @click.native='sendCode'></ziksang-count-down>
+
     </div>
 </template>
 <script>
+
 import ZiksangCountDown from './ZiksangCountDown'
+
     export default {
         name:'login1',
         components: {
@@ -17,6 +21,7 @@ import ZiksangCountDown from './ZiksangCountDown'
         },
         methods : {
             sendCode (value) {
+
                 //前面发送ajax请求成功之后调用this.start = true开始短信倒计时
                 this.start = true
             }
--- src/page/mainPage/table.vue
+++ src/page/mainPage/table.vue
@@ -2,11 +2,13 @@
   <div class="table">
     <Base1></Base1>
     <Login1></Login1>
+
   </div>
 </template>
 
 <script>
 import Base1 from '../../components/base64-md5'
+
 import Login1 from '../../components/login1'
 export default {
   name: 'table',
