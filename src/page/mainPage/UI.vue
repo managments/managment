00365d803966a034d77e5b903e212565c200e51e
liudong <1418275530@qq.com>
2017-11-02 11:41:18 +0800

    nextTick函数

--- src/page/mainPage/UI.vue
+++ src/page/mainPage/UI.vue
@@ -1,15 +1,17 @@
 <template>
   <div id="UI">
+    <NextTick></NextTick>
 <Mock></Mock>
   </div>
 </template>
 
 <script>
 import Mock from '../../components/mock'
+import NextTick from '../../components/nextTick'
 export default {
   name: 'UI',
   components:{
-    Mock
+    Mock,NextTick
   }
 }
 </script>
